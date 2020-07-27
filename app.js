50baa32f64e389ffdafaea76effa11e6e06de076
Myan Patel <myanpatel@MacBook-Pro.local>
2020-07-27 12:07:15 +0100

    Can now authenticate a user to see if username exists or passwords match

--- app.js
+++ app.js
@@ -30,6 +30,26 @@ app.route("/")
 app.route("/login")
 .get(function(req,res){
     res.render("login");
+})
+.post(function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:username}, function(err, foundUser){
+        if (err){
+            console.log(err);
+        }else{
+            if (foundUser){
+                if (foundUser.password === password){
+                    res.render("secrets");
+                }else{
+                    res.send("incorrect username and/or password");
+                }
+            }else{
+                res.send("User does not exist.");
+            }
+        }
+    });
 });
 
 app.route("/register")
