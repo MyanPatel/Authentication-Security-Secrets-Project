3b559b77e68220471be14ff70d39dad66fb4b51e
Myan Patel <myanpatel@MacBook-Pro.local>
2020-07-27 15:48:06 +0100

    Passwords are now encrypted using mongoose-encryption npm package

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const e = require("express");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -14,10 +14,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 // MongoDB/ Mongoose Setup
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true, useUnifiedTopology:true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+
+userSchema.plugin(encrypt, {secret:secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -69,7 +73,6 @@ app.route("/register")
         }
     });
 });
-
 
 app.listen(3000, function(){
     console.log("server running on port 3000");
